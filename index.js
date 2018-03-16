7ee8088cef6fb2c05807bc599cc7c524c876fda9
Zack Moore <zack.moore@izea.com>
2018-03-16 10:24:34 -0400

    working on it

--- index.js
+++ index.js
@@ -3,11 +3,15 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.send(req.body);
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('Listening.'));
