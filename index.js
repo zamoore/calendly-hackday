5a9405fe8fe4c951bd1c8c9a7df566eae09a9caa
Zack Moore <zack.moore@izea.com>
2018-03-16 10:28:37 -0400

    Changed to post request

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
   console.log(req.body);
 });
 
